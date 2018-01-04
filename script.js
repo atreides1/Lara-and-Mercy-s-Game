6034885b8526dc53dc3ef2c131a3e147b79245ff
Mercy Bhakta <mercybhakta@yahoo.com>
2018-01-04 12:37:07 -0800

    Added the ending, still a bit buggy

--- script.js
+++ script.js
@@ -3,26 +3,58 @@ let neg = 0;
 
 function updateScore()
 {
-  if (pos > neg)
+  if (pos >= neg)
   {
-    //set link to end1
+    d = document.getElementById("next");
+    p = document.createElement('a');
+    p.href = 'end1.html';
+    p.innerHTML = "Next";
+    d.appendChild(p);
   }
   else
   {
     //set link to end2
+    d = document.getElementById("next");
+    p = document.createElement('a');
+    p.href = 'end2.html';
+    p.innerHTML = "Next";
+    d.appendChild(p);
   }
   return true
-}
+};
 
 function updatePos()
 {
   pos++;
-}
+  console.log(pos);
+};
 
 function updateNeg()
 {
   neg++;
-}
+  console.log(neg);
+};
 
-var element = document.getElementById('myElement');
-element.addEventListener('click', clickHandler);
+function firstChoice()
+{
+  const p1 = document.getElementById('p1');
+  const n1 = document.getElementById('n1');
+  p1.addEventListener('click', updatePos);
+  n1.addEventListener('click', updateNeg);
+};
+
+function secondChoice()
+{
+  const p2 = document.getElementById('p2');
+  const n2 = document.getElementById('n2');
+  p2.addEventListener('click', updatePos);
+  n2.addEventListener('click', updateNeg);
+};
+
+function thirdChoice()
+{
+  const p3 = document.getElementById('p3');
+  const n3 = document.getElementById('n3');
+  p3.addEventListener('click', updatePos);
+  n3.addEventListener('click', updateNeg);
+};
