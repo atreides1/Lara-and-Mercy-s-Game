a0a24f3d0e83286070a43932e691edc3fd1572c0
nelry <stantonniles@gmail.com>
2018-01-11 16:59:42 -0500

    Add files via upload

--- serverVersion/app.js
+++ serverVersion/app.js
@@ -5,8 +5,12 @@ var io = require('socket.io')(http);
 //var oneDay = 86400000; 
 //choices made in morning.html, workWithChoices.html, party1.html
 
-var block = ["1", "0", "0"];  //0 indicates nothing blocked rest 1-3 correspond to choice blocked
+var lilaRoom = "i";
+var ghostRoom = "i";
+
+var block = ["0", "0", "0"];  //0 indicates nothing blocked rest 1-3 correspond to choice blocked
 								
+
 app.use(express.static(__dirname)); //loads the website
 
 io.on('connection', function(socket){ //listens for user events 
@@ -32,18 +36,35 @@ io.on('connection', function(socket){ //listens for user events
 	}
 });
 
-socket.on('setBlock', function(msg){ 
-	console.log('setBlock: ' + msg);
-  	if(msg == "0"){
-	block[0] = msg;
+socket.on('setBlock', function(message){ 
+	console.log('setBlock: ' + message);
+	var msg = message.split(',');
+  	if(msg[0] == "0"){
+	block[0] = msg[1];
+	io.emit('block' , block[0]);
 	}
-	if(msg == "1"){
-	block[1] = msg;
+	if(msg[0] == "1"){
+	block[1] = msg[1];
+	io.emit('block' , block[1]);
 	}
-	if(msg == "2"){
-	block[2] = msg;
+	if(msg[0] == "2"){
+	block[2] = msg[1];
+	io.emit('block' , block[2]);
 	}
 });
+
+socket.on('getRoom', function(msg){ 
+	if(msg == "lila"){
+		socket.emit("returnRoom", lilaRoom);
+		lilaRoom = lilaRoom + "i";
+	}
+	if(msg == "ghost"){
+		socket.emit("returnRoom", ghostRoom);
+		ghostRoom = ghostRoom + "i";
+	}
+   console.log('user input: ' + msg); 
+
+  });
 
 });
 http.listen(3002);
